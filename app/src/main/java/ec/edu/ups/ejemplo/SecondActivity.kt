0a5589b7f92137762b9d4aa47b9c380ced536f93
EduardoAyora <eduardoaayora64@gmail.com>
2022-10-27 20:05:38 -0500

    Agregar imagenes de preguntas

--- app/src/main/java/ec/edu/ups/ejemplo/SecondActivity.kt
+++ app/src/main/java/ec/edu/ups/ejemplo/SecondActivity.kt
@@ -24,7 +24,6 @@ class SecondActivity : AppCompatActivity() {
 
         val username = intent.getStringExtra("username")
         val txtUsername: TextView = findViewById(R.id.txtUsername)
-        txtUsername.text = "Bienvenido ${username}"
 
         val txtPregunta: TextView = findViewById(R.id.txtPregunta)
         val btnOptionOne: Button = findViewById(R.id.btnOptionOne)
@@ -38,6 +37,7 @@ class SecondActivity : AppCompatActivity() {
             btnOptionOne.setText(banco.get(indicePregunta).respuestas[0].valor)
             btnOptionTwo.setText(banco.get(indicePregunta).respuestas[1].valor)
             btnOptionThree.setText(banco.get(indicePregunta).respuestas[2].valor)
+            txtUsername.text = "Puntuación de ${username}: ${respuestasCorrectas}"
         }
 
         setQuestion()
@@ -129,79 +129,79 @@ class Respuesta(valor: String, imagenId: Int) {
 
 var banco: List<Pregunta> = listOf<Pregunta>(
     Pregunta("El Mundial de Qatar será la edición número:", listOf(
-        Respuesta("21", R.drawable.messi),
-        Respuesta("22", R.drawable.ronaldo),
-        Respuesta("23", R.drawable.ronaldo),
+        Respuesta("21", R.drawable.qatar),
+        Respuesta("22", R.drawable.qatar),
+        Respuesta("23", R.drawable.qatar),
     ) , 1),
     Pregunta("Qatar se convierte en el país número__ en albergar el Mundial", listOf(
-        Respuesta("16", R.drawable.messi),
-        Respuesta("17", R.drawable.ronaldo),
-        Respuesta("18", R.drawable.ronaldo),
+        Respuesta("16", R.drawable.qatar),
+        Respuesta("17", R.drawable.qatar),
+        Respuesta("18", R.drawable.qatar),
     ) , 2),
     Pregunta("¿Cuántos estadios habrán en el Mundial de Qatar?", listOf(
-        Respuesta("8", R.drawable.messi),
-        Respuesta("9", R.drawable.ronaldo),
-        Respuesta("10", R.drawable.ronaldo),
+        Respuesta("8", R.drawable.estadio),
+        Respuesta("9", R.drawable.estadio),
+        Respuesta("10", R.drawable.estadio),
     ) , 0),
     Pregunta("¿Cuál es la selección con mas copas mundiales ganadas?", listOf(
-        Respuesta("Brasil", R.drawable.messi),
-        Respuesta("Alemania", R.drawable.ronaldo),
-        Respuesta("Argentina", R.drawable.ronaldo),
+        Respuesta("Brasil", R.drawable.copa_mundial),
+        Respuesta("Alemania", R.drawable.copa_mundial),
+        Respuesta("Argentina", R.drawable.copa_mundial),
     ) , 0),
     Pregunta("¿Quién es el jugador latino que ha anotado más goles en la historia de los mundiales?", listOf(
-        Respuesta("Gabriel Batistuta", R.drawable.messi),
-        Respuesta("Ronaldo Nazario", R.drawable.ronaldo),
-        Respuesta("Diego Maradona", R.drawable.ronaldo),
+        Respuesta("Gabriel Batistuta", R.drawable.ronaldo_nazario),
+        Respuesta("Ronaldo Nazario", R.drawable.ronaldo_nazario),
+        Respuesta("Diego Maradona", R.drawable.ronaldo_nazario),
     ) , 1),
     Pregunta("¿Cómo se llamó la mascota de la Copa Mundial de 1982 en España?", listOf(
-        Respuesta("Juanito", R.drawable.messi),
-        Respuesta("Fuleco", R.drawable.ronaldo),
-        Respuesta("Naranjito", R.drawable.ronaldo),
+        Respuesta("Juanito", R.drawable.naranjito),
+        Respuesta("Fuleco", R.drawable.naranjito),
+        Respuesta("Naranjito", R.drawable.naranjito),
     ) , 2),
     Pregunta("Contra que equipo Diego Maradona hizo el gol con “la mano de Dios”", listOf(
-        Respuesta("Italia", R.drawable.messi),
-        Respuesta("Alemania", R.drawable.ronaldo),
-        Respuesta("Inglaterra", R.drawable.ronaldo),
+        Respuesta("Italia", R.drawable.maradona),
+        Respuesta("Alemania", R.drawable.maradona),
+        Respuesta("Inglaterra", R.drawable.maradona),
     ) , 2),
     Pregunta("El goleador del primer Mundial de fútbol, disputado en 1930 en Uruguay, fue…", listOf(
-        Respuesta("Bigode", R.drawable.messi),
-        Respuesta("Alcides Ghiggia", R.drawable.ronaldo),
-        Respuesta("Guillermo Stábile", R.drawable.ronaldo),
+        Respuesta("Bigode", R.drawable.guillermo_stabile),
+        Respuesta("Alcides Ghiggia", R.drawable.guillermo_stabile),
+        Respuesta("Guillermo Stábile", R.drawable.guillermo_stabile),
     ) , 2),
     Pregunta("¿Cómo se le llamó a la famosa victoria de Uruguay sobre Brasil en la final de 1950?", listOf(
-        Respuesta("El triunfo del siglo", R.drawable.messi),
-        Respuesta("El Maracanazo", R.drawable.ronaldo),
-        Respuesta("El Matabrasileirao", R.drawable.ronaldo),
+        Respuesta("El triunfo del siglo", R.drawable.maracanazo),
+        Respuesta("El Maracanazo", R.drawable.maracanazo),
+        Respuesta("El Matabrasileirao", R.drawable.maracanazo),
     ) , 1),
     Pregunta("¿Sabes qué país tenía que organizar el Mundial de 1986 y tuvo que renunciar?", listOf(
-        Respuesta("Argentina", R.drawable.messi),
-        Respuesta("Colombia", R.drawable.ronaldo),
-        Respuesta("Uruguay", R.drawable.ronaldo),
+        Respuesta("Argentina", R.drawable.colombia),
+        Respuesta("Colombia", R.drawable.colombia),
+        Respuesta("Uruguay", R.drawable.colombia),
     ) , 1),
     Pregunta("¿Cuál es el jugador latino que, junto al alemán Lothar Matthaeus, tiene más participaciones en el mundial?", listOf(
-        Respuesta("Pelé (Brasil)", R.drawable.messi),
-        Respuesta("Javier Zanetti (Argentina)", R.drawable.ronaldo),
-        Respuesta("Antonio Carbajal (México)", R.drawable.ronaldo),
+        Respuesta("Pelé (Brasil)", R.drawable.antonio_carbajal),
+        Respuesta("Javier Zanetti (Argentina)", R.drawable.antonio_carbajal),
+        Respuesta("Antonio Carbajal (México)", R.drawable.antonio_carbajal),
     ) , 2),
     Pregunta("¿Dónde se disputó la Copa Mundial de 1998?", listOf(
-        Respuesta("Italia", R.drawable.messi),
-        Respuesta("Francia", R.drawable.ronaldo),
-        Respuesta("Estados Unidos", R.drawable.ronaldo),
+        Respuesta("Italia", R.drawable.francia),
+        Respuesta("Francia", R.drawable.francia),
+        Respuesta("Estados Unidos", R.drawable.francia),
     ) , 1),
     Pregunta("La selección de fútbol de Cuba participó en una Copa del Mundo. ¿Sabes en qué año fue?", listOf(
-        Respuesta("Suiza 1954", R.drawable.messi),
-        Respuesta("Francia 1938", R.drawable.ronaldo),
-        Respuesta("México 1970", R.drawable.ronaldo),
+        Respuesta("Suiza 1954", R.drawable.francia),
+        Respuesta("Francia 1938", R.drawable.francia),
+        Respuesta("México 1970", R.drawable.francia),
     ) , 1),
     Pregunta("¿Quién fue el jugador que marcó el gol que le dio el triunfo a España en la final de la Copa del Mundo del 2010 en Sudáfrica?", listOf(
-        Respuesta("Andrés Iniesta", R.drawable.messi),
-        Respuesta("David Villa", R.drawable.ronaldo),
-        Respuesta("Sergio Ramos", R.drawable.ronaldo),
+        Respuesta("Andrés Iniesta", R.drawable.iniesta),
+        Respuesta("David Villa", R.drawable.iniesta),
+        Respuesta("Sergio Ramos", R.drawable.iniesta),
     ) , 0),
     Pregunta("¿Cuál es la selección que más finales de la Copa Mundial ha disputado?", listOf(
-        Respuesta("Brasil", R.drawable.messi),
-        Respuesta("Alemania", R.drawable.ronaldo),
-        Respuesta("Italia", R.drawable.ronaldo),
+        Respuesta("Brasil", R.drawable.alemania),
+        Respuesta("Alemania", R.drawable.alemania),
+        Respuesta("Italia", R.drawable.alemania),
     ) , 1),
 )
 
